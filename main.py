2d5432ff9b3f082d83031cc8b09a284097781e77
Shrey Gupta <shhr3y@gmail.com>
2022-09-27 23:42:31 -0500

    testing

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
 import eventlet
 import socketio
-import socket
-import threading
+import json
+import dataclasses
+from typing import List, Mapping, Optional, Tuple, Union
+import mediapipe as mp
+import plotly.express as px
+from pathlib import Path
+import pandas as pd
+import plotly.graph_objects as go
+import numpy as np
+import dash
+from dash import html
+from dash import dcc
+
 
 host = '192.168.0.103'
 port = 9876	
+
+mp_pose = mp.solutions.pose
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
+
+_PRESENCE_THRESHOLD = 0.5
+_VISIBILITY_THRESHOLD = 0.5
 
 sio = socketio.Server(cors_allowed_origins="*", async_mode='eventlet')
 app = socketio.WSGIApp(sio)
@@ -16,78 +34,93 @@ app = socketio.WSGIApp(sio)
 #     sio.emit('ping')
 
 
-@sio.on('ping')
+@sio.on('landmarks')
 def ping(*args):
-    print("received ping - send pong")
-    sio.emit('pong')
+    landmarks = args[1]
+    json_list = []
+    for landmark in landmarks:
+        json_object = json.loads(landmark)
+        json_list.append(json_object)
+
+    fig.update(plot_landmarks(json_list,  mp_pose.POSE_CONNECTIONS))
 
 
-# ping_in_intervals()
-eventlet.wsgi.server(eventlet.listen((host, port)), app)
 
 
 
+fig = go.Figure()
 
-# import numpy as np
-# import socket
+def plot_landmarks(landmark_list, connections=None):
+    if not landmark_list:
+        return
+    
+    plotted_landmarks = {}
+    for idx, landmark in enumerate(landmark_list):
+        if (landmark['visibility'] < _VISIBILITY_THRESHOLD) or (landmark['presence'] < _PRESENCE_THRESHOLD):
+            continue
 
-# import sys
+        plotted_landmarks[idx] = (-landmark['z'], landmark['x'], -landmark['y'])
+    if connections:
+        out_cn = []
+        num_landmarks = len(landmark_list)
+        # Draws the connections if the start and end landmarks are both visible.
+        for connection in connections:
+            start_idx = connection[0]
+            end_idx = connection[1]
+            if not (0 <= start_idx < num_landmarks and 0 <= end_idx < num_landmarks):
+                raise ValueError(
+                    f"Landmark index is out of range. Invalid connection "
+                    f"from landmark #{start_idx} to landmark #{end_idx}."
+                )
+            if start_idx in plotted_landmarks and end_idx in plotted_landmarks:
+                landmark_pair = [
+                    plotted_landmarks[start_idx],
+                    plotted_landmarks[end_idx],
+                ]
+                out_cn.append(
+                    dict(
+                        xs=[landmark_pair[0][0], landmark_pair[1][0]],
+                        ys=[landmark_pair[0][1], landmark_pair[1][1]],
+                        zs=[landmark_pair[0][2], landmark_pair[1][2]],
+                    )
+                )
+            
+        cn2 = {"xs": [], "ys": [], "zs": []}
+        for pair in out_cn:
+            for k in pair.keys():
+                cn2[k].append(pair[k][0])
+                cn2[k].append(pair[k][1])
+                cn2[k].append(None)
 
-# host = socket.gethostbyname(socket.gethostname())
-# port = 9876		
+    df = pd.DataFrame(plotted_landmarks).T.rename(columns={0: "z", 1: "x", 2: "y"})
+    df["lm"] = df.index.map(lambda s: mp_pose.PoseLandmark(s).name).values
+    figure = (
+        px.scatter_3d(df, x="z", y="x", z="y", hover_name="lm")
+        .update_traces(marker={"color": "red"})
+        .update_layout(
+            margin={"l": 0, "r": 0, "t": 0, "b": 0},
+            scene={"camera": {"eye": {"x": 2.1, "y": 0, "z": 0}}},
+        )
+    )
+    figure.add_traces(
+        [
+            go.Scatter3d(
+                x=cn2["xs"],
+                y=cn2["ys"],
+                z=cn2["zs"],
+                mode="lines",
+                line={"color": "black", "width": 5},
+                name="connections",
+            )
+        ]
+        )
+    return figure
 
-
-# class VideoStreamingTest(object):
-#     def __init__(self, host, port):
-#         print('Trying socket on', host, port)
-#         try:
-#             self.server_socket = socket.socket()
-#         except socket.error as message:
-#             print('Bind failed. Error Code : ' + str(message[0]) + ' Message ' + message[1])
-#             sys.exit()
-#         print('Bind successful on socket', host, port)
-#         self.server_socket.bind((host, port))
-#         self.server_socket.listen(0)
-#         self.connection, self.client_address = self.server_socket.accept()
-
-#         # self.connection = self.connection.makefile('rb')
-#         self.host_name = socket.gethostname()
-#         self.host_ip = socket.gethostbyname(self.host_name)
-
-
-#         # self.streaming()
-
-#     @socket.on('ping')
-#     def ping(*args):
-#         print("received ping - send pong")
-#         socket.emit('pong')
-
-#     def streaming(self):
-#         try:
-#             print("Host: ", self.host_name + ' ' + self.host_ip)
-#             print("Connection from: ", self.client_address)
-#             print("Streaming...")
-#             print("Press 'q' to exit")
-
-#             # need bytes here
-#             stream_bytes = b' '
-#             while True:
-#                 stream_bytes += self.connection.read(1024)
-#                 first = stream_bytes.find(b'\xff\xd8')
-#                 last = stream_bytes.find(b'\xff\xd9')
-#                 if first != -1 and last != -1:
-#                     jpg = stream_bytes[first:last + 2]
-#                     stream_bytes = stream_bytes[last + 2:]
-#                     image = cv2.imdecode(np.frombuffer(jpg, dtype=np.uint8), cv2.IMREAD_COLOR)
-#                     cv2.imshow('image', image)
-
-#                     if cv2.waitKey(1) & 0xFF == ord('q'):
-#                         break
-#         finally:
-#             self.connection.close()
-#             self.server_socket.close()
-
-
-# if __name__ == '__main__':
-#     # host, port
-#     VideoStreamingTest(host, port)
+app = dash.Dash()
+app.layout = html.Div([
+    dcc.Graph(figure=fig)
+])
+    
+if __name__ == '__main__':
+    app.run_server(debug=True, use_reloader=False)
+    eventlet.wsgi.server(eventlet.listen((host, port)), app)
