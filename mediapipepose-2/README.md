3bf1b9f148d97f55adf55a861d36ccb448ff8629
Shrey Gupta <shhr3y@gmail.com>
2022-09-29 01:36:49 -0500

    working in 2d

--- mediapipepose-2/README.md
+++
@@ -1,4 +0,0 @@
-# MediaPipe - Pose
-
-A Pen created on CodePen.io. Original URL: [https://codepen.io/shhr3y/pen/QWrQbVV](https://codepen.io/shhr3y/pen/QWrQbVV).
-
--- server/script.js
+++ server/script.js
@@ -1,36 +1,36 @@
+const mpPose = window;
+const landmarkContainer = document.body.getElementsByClassName('landmark-grid-container')[0];
+
+const grid = new LandmarkGrid(landmarkContainer, {
+  connectionColor: 0xCCCCCC,
+  definedColors:
+      [{name: 'LEFT', value: 0xffa500}, {name: 'RIGHT', value: 0x00ffff}],
+  range: 2,
+  fitToGrid: true,
+  labelSuffix: 'm',
+  landmarkSize: 2,
+  numCellsPerAxis: 4,
+  showHidden: false,
+  centered: true,
+});
+
 var client = new io('192.168.0.103:9876', { 
    transports : ['websocket'] });
 
-var counter = 0
 client.on("connect", () => {
-  counter += 1
-  console.log('connected', counter, client.id);
+  console.log('Connection Established to Socket!', client.id);
 });
 
 client.on('render', function (data) {
   let landmarks = data['data']
-  updateLandmarks(landmarks)
-});
-
-
-const landmarkContainer = document.body.getElementsByClassName('landmark-grid-container')[0];
-const grid = new LandmarkGrid(landmarkContainer, {
-connectionColor: 0xCCCCCC,
-definedColors:
-    [{name: 'LEFT', value: 0xffa500}, {name: 'RIGHT', value: 0x00ffff}],
-range: 2,
-fitToGrid: true,
-labelSuffix: 'm',
-landmarkSize: 2,
-numCellsPerAxis: 4,
-showHidden: false,
-centered: true,
-});
-
-function updateLandmarks(landmarks) {
+  
   if (landmarks) {
-      grid.updateLandmarks(landmarks);
+    grid.updateLandmarks(landmarks, mpPose.POSE_CONNECTIONS, [
+      { list: Object.values(mpPose.POSE_LANDMARKS_LEFT), color: 'LEFT' },
+      { list: Object.values(mpPose.POSE_LANDMARKS_RIGHT), color: 'RIGHT' },
+    ]);
   } else {
-  grid.updateLandmarks([]);
+    grid.updateLandmarks([]);
   }
-}
+});
+
