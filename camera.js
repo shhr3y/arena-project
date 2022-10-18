e4707eb68b5502ae0dae4009c163cc752403d932
Shrey Gupta <shhr3y@gmail.com>
2022-10-17 23:52:23 -0500

    working

---
+++ camera.js
@@ -0,0 +1,32 @@
+const videoElement = document.getElementsByClassName('input_video')[0];
+const landmarkContainer = document.getElementsByClassName('landmark-grid-container')[0];
+const grid = new LandmarkGrid(landmarkContainer);
+
+function onResults(results) {
+  if (!results.poseLandmarks) {
+    grid.updateLandmarks([]);
+    return;
+  }
+
+  grid.updateLandmarks(results.poseWorldLandmarks);
+}
+
+const pose = new Pose({locateFile: (file) => {
+  return `https://cdn.jsdelivr.net/npm/@mediapipe/pose/${file}`;
+}});
+pose.setOptions({
+  modelComplexity: 1,
+  smoothLandmarks: true,
+  minDetectionConfidence: 0.5,
+  minTrackingConfidence: 0.5
+});
+pose.onResults(onResults);
+
+const camera = new Camera(videoElement, {
+  onFrame: async () => {
+    await pose.send({image: videoElement});
+  },
+  width: 1280,
+  height: 720
+});
+camera.start();
--- xr.py
+++ xr.py
@@ -1,29 +1,61 @@
-import socketio
+from arena import *
 
-sio = socketio.Client()
-print('Created socketio client')
+scene = Scene(host="mqtt.arenaxr.org", scene="myfirstscene")
 
-@sio.event
-def connect():
-    print('connected to server')
+skeleton = Object(object_id)
 
-@sio.event
-def disconnect():
-    print('disconnected from server')
+right_wrist_shape = Sphere(
+        object_id="right_wrist",
+        scale=(0.2,0.2,0.2),
+        color=(30,20,10),
+    )
+left_wrist_shape = Sphere(
+        object_id="left_wrist",
+        scale=(0.2,0.2,0.2),
+        color=(70,0,100),
+    )
 
-@sio.on('render')
-def render(*args):
-    # landmarks = args[1]
-    print(args)
-
-sio.connect('ws://192.168.0.106:9876/')
-sio.wait()
-
-
-
-
+head_shape = Sphere(
+        object_id="head",
+        scale=(0.4,0.4,0.4),
+        color=(0,255,255),
+    )
 
 
+
+# update_box(landmarks[0])
+
+@scene.run_once
+def add_shapes():
+    scene.add_object(right_wrist_shape)
+    scene.add_object(left_wrist_shape)
+    scene.add_object(head_shape)
+
+def update_skeleton(landmarks):
+    update_head(landmarks[0])
+    update_right_wrist(landmarks[15])
+    update_left_wrist(landmarks[16])
+
+def update_head(coordinates):
+    head_shape.update_attributes(position=get_position(coordinates))
+    scene.update_object(head_shape)
+
+def update_right_wrist(coordinates):
+    right_wrist_shape.update_attributes(position=get_position(coordinates))
+    scene.update_object(right_wrist_shape)
+
+def update_left_wrist(coordinates):
+    left_wrist_shape.update_attributes(position=get_position(coordinates))
+    scene.update_object(left_wrist_shape)
+
+multplier = 3
+def get_position(coordinates):
+    return Position(multplier*coordinates[0], -multplier*coordinates[1], multplier*coordinates[2])
+
+    
+
+if __name__ == "__main__":
+    scene.run_tasks()
 
 
 
